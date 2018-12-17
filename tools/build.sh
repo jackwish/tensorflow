a34ab5e4b45201617d682466ef8b19fef6bafee3
王振华 (WANG Zhenhua) <i@jackwish.net>
2018-12-17 08:19:31 +0000

    build archive library

--- tools/build.sh
+++ tools/build.sh
@@ -1,15 +1,15 @@
 export CC=gcc-4.8
 export CXX=g++-4.8
 
-# build android demo apk
+########### build tflite android demo apk ##################################
 # bazel build -c opt --cxxopt='--std=c++11' --fat_apk_cpu=x86,x86_64,arm64-v8a,armeabi-v7a \
 #     //tensorflow/contrib/lite/examples/android:tflite_demo
 
 
 ########### build tflite shared library ####################################
 
-# build native
-bazel build -c opt //tensorflow/contrib/lite:libtflite.so --cxxopt="-std=c++11"
+# build shared library native
+# bazel build -c opt -c dbg //tensorflow/contrib/lite:libtflite.so --cxxopt="-std=c++11"
 
 # build armv8
 # bazel build -c opt //tensorflow/contrib/lite:libtflite.so \
@@ -19,3 +19,7 @@ bazel build -c opt //tensorflow/contrib/lite:libtflite.so --cxxopt="-std=c++11"
 #   --cpu=arm64-v8a \
 #   --fat_apk_cpu=arm64-v8a \
 #   --cxxopt="-std=c++14"
+
+########### build tflite archive and brenchmark ############################
+./tensorflow/contrib/lite/tools/make/download_dependencies.sh
+make -f ./tensorflow/contrib/lite/tools/make/Makefile -j8
